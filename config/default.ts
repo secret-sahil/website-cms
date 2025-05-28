fb4f79c8c2ef82653a2944bb9fc14159a33145df
Sahil Kumar <sahilkumar142002@gmail.com>
2025-05-28 10:41:49 +0530

    feat: update CORS origins to include production domains

--- config/default.ts
+++ config/default.ts
@@ -4,7 +4,12 @@ export default {
   redisCacheExpiresIn: 30, //days
   refreshTokenExpiresIn: 30, //days
   accessTokenExpiresIn: 1, //days
-  origin: ['http://localhost:3000'],
+  origin: [
+    'http://localhost:3000',
+    'https://infutrix.com',
+    'https://www.infutrix.com',
+    'https://admin.infutrix.com',
+  ],
   // images upload
   cloudfrontBaseUrl: 'https://d26yyyedhrucxb.cloudfront.net',
 };
