6a1dae2e4c8e78f439ae7539d6d646445d47af62
Sahil Kumar <sahilkumar142002@gmail.com>
2025-06-29 01:19:05 +0530

    refactor: migrate configuration files from JSON to TypeScript format

---
+++ dist/config/custom-environment-variables.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    port: 'PORT',
+    env: 'NODE_ENV',
+    accessTokenPrivateKey: 'JWT_ACCESS_TOKEN_PRIVATE_KEY',
+    accessTokenPublicKey: 'JWT_ACCESS_TOKEN_PUBLIC_KEY',
+    refreshTokenPrivateKey: 'JWT_REFRESH_TOKEN_PRIVATE_KEY',
+    refreshTokenPublicKey: 'JWT_REFRESH_TOKEN_PUBLIC_KEY',
+    redisUrl: 'REDIS_URL',
+    smtp: {
+        service: 'EMAIL_HOST',
+        user: 'EMAIL_USER',
+        pass: 'EMAIL_PASS',
+    },
+    awsRegion: 'AWS_REGION',
+    awsAccessKeyID: 'AWS_ACCESS_KEY_ID',
+    awsSecretAccessKey: 'AWS_SECRET_ACCESS_KEY',
+    awsBucketName: 'AWS_S3_BUCKET_NAME',
+    dataEncryptionKey: 'DATA_ENCRYPTION_KEY',
+};
+//# sourceMappingURL=custom-environment-variables.js.map
---
+++ dist/config/default.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    appName: 'Bytelog Solutions (Development)',
+    verificationTokenExpiresIn: 10, //minutes
+    redisCacheExpiresIn: 30, //days
+    refreshTokenExpiresIn: 30, //days
+    accessTokenExpiresIn: 1, //days
+    origin: ['http://localhost:3000'],
+    // images upload
+    cloudfrontBaseUrl: 'https://d26yyyedhrucxb.cloudfront.net',
+};
+//# sourceMappingURL=default.js.map
